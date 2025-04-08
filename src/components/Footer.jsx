7f99a4696ee53219466139b2d11ce552be28441e
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-04-08 10:45:23 +0000

    update copyright year in Footer component to use dynamic current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="bg-gray-900 pb-4">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4 text-white">
@@ -214,7 +215,7 @@ const Footer = () => {
         {/* copyright section */}
         <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
           <p className="text-sm text-gray-500">
-            © Copyright 2023 | All right reserved.
+            © Copyright {currentYear} | All right reserved.
           </p>
           <div className="flex items-center mt-4 space-x-4 sm:mt-0">
             <a
